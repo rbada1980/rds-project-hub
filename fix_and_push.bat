ac7fc15ed72485c69c356cecbb3274f3f7a74579
rbada1980 <rbada1980@gmail.com>
2026-07-01 11:07:59 +0530

    analytics file

--- fix_and_push.bat
+++ fix_and_push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Users\HP\rds-project-hub
-echo Pushing mobile UI v5 fixes...
+echo Pushing analytics Excel export upgrade...
 git push origin main
 echo.
 echo Done! Vercel deploys in ~1 min.
